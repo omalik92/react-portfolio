800a33aa06eccf16688c431e6bfc4aace6626257
Omar <omalik0192@googlemail.com>
2023-03-03 19:35:11 +0000

    initial scaffolding added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        OZ Technologies.
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
